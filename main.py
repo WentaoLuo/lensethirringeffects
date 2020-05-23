d0bbfdd0b0b6ca341edeee3b63e212f84a0120d6
WentaoLuo <wentao.luo@yale.edu>
2020-05-23 13:42:47 +0900

    middle version

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
 #!/home/wtluo/anaconda/bin/python2.7
 
 from pylab import *
@@ -31,14 +37,14 @@ def plot_shears_kappa(kappa,shear1,shear2):
            st_y = j-ampli*np.sin(alph)*scale_shear
            md_y = j
            ed_y = j+ampli*np.sin(alph)*scale_shear
-           plt.plot([md_x,ed_x],[md_y,ed_y],'w-')
-           plt.plot([md_x,st_x],[md_y,st_y],'w-')
+           plt.plot([md_x,ed_x],[md_y,ed_y],'k-',linewidth=2)
+           plt.plot([md_x,st_x],[md_y,st_y],'k-',linewidth=2)
 
     plt.xlim(0,64)
     plt.ylim(0,64)
 #----------------------------------------------------
-def main():
-  xi  = np.linspace(50,300,50)
+def SIS():
+  xi  = np.linspace(0.01,10,10)
   xi1 = np.linspace(-32,32,64)
   xi2 = np.linspace(-32,32,64)
 
@@ -46,7 +52,37 @@ def main():
   vrot= 0.0
   zl  = 0.2
   zs  = 0.4
+  noise = True
+  #noise = False
+  wlsis= wl.weaksis(vrot,vdis,zl,zs)
+  esd  = wlsis.sisESD(xi,noise)
+  plt.errorbar(esd['Rp'],esd['ESD'],yerr=esd['ERROR'],fmt='k.',ms=15,elinewidth=2.5)
+  #plt.yscale('log')
+  #plt.xscale('log')
+  plt.xlabel(r'$\mathrm{R}(Mpc/h)$')
+  plt.ylabel(r'$\mathrm{ESD(M_{\odot}h/pc^2)}$')
+  plt.show()
 
+  #plt.ylabel(r'$\mathrm{ESD(M_{\odot}h/pc^2)}$')
+  kap  = wlsis.sisKappa(xi1,xi2,noise)
+  gm   = wlsis.sisShear(xi1,xi2,noise)
+  shear1 = gm['f_1']
+  shear2 = gm['f_2']
+  plot_shears_kappa(kap,shear1,shear2)
+  plt.xlim(0,64)
+  plt.ylim(0,64)
+  plt.show()
+def GRM():
+  xi1 = np.linspace(-32,32,64)
+  xi2 = np.linspace(-32,32,64)
+  xi=np.sqrt(xi1*xi1+xi2*xi2)*pi/180.0/60.0
+  vdis= 300.0
+  #phi= np.pi/2
+  #phi= np.pi/4
+  phi= 0
+  vrot= 0.0
+  zl  = 0.2
+  zs  = 0.4
   wlsis= wl.weaksis(vrot,vdis,zl,zs)
   esd  = wlsis.sisESD(xi)
   #plt.plot(xi,esd)
@@ -54,15 +90,40 @@ def main():
   #plt.yscale('log')
   #plt.xlabel(r'$\mathrm{\xi}(arcmin)$')
   #plt.ylabel(r'$\mathrm{ESD(M_{\odot}h/pc^2)}$')
-  kap  = wlsis.sisKappa(xi1,xi2)
-  gm   = wlsis.sisShear(xi1,xi2)
+  kap  = wlsis.GRMKappa(xi1,xi2,phi)
+  gm   = wlsis.GRMShear(xi1,xi2,phi)
   shear1 = gm['g_1']
   shear2 = gm['g_2']
   plot_shears_kappa(kap,shear1,shear2)
   plt.xlim(0,64)
   plt.ylim(0,64)
   plt.show()
+def Total():
+  xi1 = np.linspace(-32,32,64)
+  xi2 = np.linspace(-32,32,64)
+  #xi=np.sqrt(xi1*xi1+xi2*xi2)*pi/180.0/60.0
+  xi  = np.logspace(10,300,10)
+  vdis= 300.0
+  #phi= np.pi/2
+  #phi= np.pi/4
+  phi= 0
+  vrot= 0.0
+  zl  = 0.2
+  zs  = 0.4
+  wlsis= wl.weaksis(vrot,vdis,zl,zs)
+  noise= True 
+  kap  = wlsis.TotalKappa(xi1,xi2,phi,noise)
+  gm   = wlsis.TotalShear(xi1,xi2,phi,noise)
+  shear1 = gm['h_1']
+  shear2 = gm['h_2']
+  plot_shears_kappa(kap,shear1,shear2)
+  plt.xlim(0,64)
+  plt.ylim(0,64)
+  plt.show()
+  
 
 if __name__=="__main__":
-   main()
-
+    #SIS()
+    #GRM()
+    Total()
+   
--- weaksis.py
+++ weaksis.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 20:54:35 2020
+
+@author: tcf12
+"""
+
 #--weak lensing from SIS density profile----------
 import numpy as np
 from scipy import integrate 
@@ -23,8 +30,11 @@ Ok0    = 1.0-Om0-Ol0
 rho_crt0= 2.78e11                # M_sun Mpc^-3 *h*h 
 rho_bar0= rho_crt0*Om0       # M_sun Mpc^-3 *h*h
 pi      = np.pi
+lamda =0.5
 fac   =(vc*vc*ckg)/(4.*pi*Gg*ckm)
 
+shapen     = 0.01
+ngdens     = 50.0
 class weaksis(object):
   def __init__(self,vrot=None,vdis=None,zl=None,zs=None):
      self.vrot=vrot
@@ -48,36 +58,135 @@ class weaksis(object):
      res = self.Dh()*integrate.romberg(self.efunclcdm, 0, x)
      return res
 #----end of cosmology functions---------------------------------
- 
-  def sisESD(self,xi):
-     dl  = self.Da(self.zl)
-     ds  = self.Da(self.zs)
-     Sig = fac*ds/(dl*(ds-dl))/(1.0+self.zl)/(1.0+self.zl)
-     ESD = Sig*self.vdis**2*ckg/(2.0*Gg*ckm*dl*xi*pi/180.0/60.0) 
-     return ESD
-  def sisKappa(self,xi1,xi2):
+  def sisESD(self,xi,noise):
+     nbins = len(xi)
+     dl    = self.Da(self.zl)
+     ds    = self.Da(self.zs)
+     theta = dl*((0.5*xi[1:nbins]+0.5*xi[0:nbins-1])*pi/180.0/60.0)
+     Sigc  = fac*ds/(dl*(ds-dl))/(1.0+self.zl)/(1.0+self.zl)
+     gm    = self.vdis**2*ckg/(2.0*Gg*ckm*Sigc*theta)
+     ngals = (pi*(xi[1:nbins]**2-xi[0:nbins-1]**2)*ngdens)
+     error = (gm*0.0+shapen)/np.sqrt(ngals)
+     if noise==False:
+       tmp = Sigc*gm
+     if noise==True:
+       tmp = np.zeros(nbins-1)
+       for j in range(nbins-1):
+         gmt    = np.random.normal(loc=gm[j],scale=shapen,size=int(np.round(ngals[j])))
+         tmp[j] = Sigc*np.mean(gmt)
+     res={'Rp':theta,'ESD':tmp,'ERROR':Sigc*error}
+     return res 
+  def Xi0(self,):
+      dl  = self.Da(self.zl)
+      ds  = self.Da(self.zs)
+      xi0=4*pi*(self.vdis*self.vdis)*dl*(ds-dl)/(vc*vc)/ds
+      return xi0
+
+  def sisKappa(self,xi1,xi2,noise):
      xv,yv  = np.meshgrid(xi1,xi2)
+     nx  = len(xi1)
+     ny  = len(xi2)
      xv  = xv*pi/180.0/60.0
      yv  = yv*pi/180.0/60.0
      dl  = self.Da(self.zl)
      ds  = self.Da(self.zs)
-     Sig = fac*ds/(dl*(ds-dl))/(1.0+self.zl)/(1.0+self.zl)
      Sigc= fac*ds/(dl*(ds-dl))/(1.0+self.zl)/(1.0+self.zl)
+     ngals= int(np.round(((4.0*np.max(xi1)*np.max(xi2))/float(nx)/float(ny))*ngdens))
      kap = self.vdis**2*ckg/(2.0*Gg*ckm*Sigc*dl*np.sqrt(xv*xv+yv*yv))
-
+     if noise==False:
+       kap = kap
+     if noise==True:
+       tmp = np.zeros((nx,ny))
+       for i in range(ny):
+         for j in range(ny):
+           gmt    = np.random.normal(loc=kap[i,j],scale=shapen,size=ngals)
+           kap[i,j] = np.mean(gmt)
+     
      return kap 
-  def sisShear(self,xi1,xi2):
+ 
+  def sisShear(self,xi1,xi2,noise):
      xv,yv  = np.meshgrid(xi1,xi2)
-     kappa  = self.sisKappa(xi1,xi2)
+     kappa  = self.sisKappa(xi1,xi2,noise)
      the2   = (xv*xv+yv*yv)
      cph    = (xv*xv-yv*yv)/the2
      sph    = 2.0*xv*yv/the2
      
      gm1    = kappa*cph 
      gm2    = kappa*sph 
-	
+     return {'f_1':gm1,'f_2':gm2}
+ 
+  def GRMKappa(self,xi1,xi2,phi):
+     xv,yv  = np.meshgrid(xi1,xi2)
+     xv  = xv*pi/180.0/60.0
+     yv  = yv*pi/180.0/60.0
+     dl  = self.Da(self.zl)
+     Rsis= self.Xi0()
+     w = 9*lamda*self.vdis/Rsis
+     w1=w*np.cos(phi)
+     w2=w*np.sin(phi)
+     noise=False
+     kap=self.sisKappa(xi1,xi2,noise)
+     kap1 = kap*dl*(w2*xv-w1*yv)/vc
+     return kap1 
+  
+  def GRMShear(self,xi1,xi2,phi):
+     xv,yv  = np.meshgrid(xi1,xi2)
+     xv  = xv*pi/180.0/60.0
+     yv  = yv*pi/180.0/60.0
+     dl  = self.Da(self.zl)
+     Rsis= self.Xi0()
+     w = 9*lamda*self.vdis/Rsis
+     w1=w*np.cos(phi)
+     w2=w*np.sin(phi)
+     noise=False
+     kappa  = self.sisKappa(xi1,xi2,noise)
+     the  = np.sqrt(xv*xv+yv*yv)
+     gm1 = kappa*dl*(w2*xv**3+w1*yv**3+3*xv*yv*(w1*xv+w2*yv))/(3*vc*the*the)
+     gm2 = 2*kappa*dl*(w2*yv**3-w1*xv**3)/(3*vc*the*the)
      return {'g_1':gm1,'g_2':gm2}
-
-
-
-
+ 
+  def TotalKappa(self,xi1,xi2,phi,noise):
+     nx  = len(xi1)
+     ny  = len(xi2)
+     kappa  = self.sisKappa(xi1,xi2,False)
+     GRkappa= self.GRMKappa(xi1,xi2,phi)
+     ngals= int(np.round(((4.0*np.max(xi1)*np.max(xi2))/float(nx)/float(ny))*ngdens))
+     Totalkappa=kappa+GRkappa
+     if noise==False:
+       Totalkappa = Totalkappa
+     if noise==True:
+       tmp = np.zeros((nx,ny))
+       for i in range(ny):
+         for j in range(ny):
+           gmt    = np.random.normal(loc=Totalkappa[i,j],scale=shapen,size=ngals)
+           Totalkappa[i,j] = np.mean(gmt)
+     return  Totalkappa
+ 
+  def TotalShear(self,xi1,xi2,phi,noise):
+     
+    nx  = len(xi1)
+    ny  = len(xi2)
+    gmsis=self.sisShear(xi1,xi2,False)
+    gmsis1=gmsis['f_1']
+    gmsis2=gmsis['f_2']
+    
+    ngals= int(np.round(((4.0*np.max(xi1)*np.max(xi2))/float(nx)/float(ny))*ngdens))
+    gmGRM=self.GRMShear(xi1,xi2,phi)
+    gmGRM1=gmGRM['g_1']
+    gmGRM2=gmGRM['g_2']
+    
+    gm1=gmsis1+gmGRM1
+    gm2=gmsis2+gmGRM2
+    if noise==False:
+       gm1 = gm1
+       gm2 = gm2
+    if noise==True:
+       tmp1 = np.zeros((nx,ny))
+       tmp2 = np.zeros((nx,ny))
+       for i in range(ny):
+         for j in range(ny):
+           gmt1    = np.random.normal(loc=gm1[i,j],scale=shapen,size=ngals)
+           gmt2    = np.random.normal(loc=gm2[i,j],scale=shapen,size=ngals)
+           gm1[i,j] = np.mean(gmt1)
+           gm2[i,j] = np.mean(gmt2)
+    return {'h_1':gm1,'h_2':gm2}
